0b058e52ec4e6c912892ffc2b20998a4630d58ca
EC2 Default User <ec2-user@ip-172-31-3-171.ap-northeast-1.compute.internal>
2022-08-07 14:12:20 +0000

    Ruby　4章まで

---
+++ practice.rb
@@ -0,0 +1,38 @@
+#puts "hello ruby"
+
+#puts 5
+#puts "5"
+
+#puts 5 + 3
+#puts "5+3"
+#puts "5"+ "3"
+
+#puts "Samの年齢は" + 27.to_s + "です"
+
+#puts 100 + "200".to_i
+
+#webcamp ="プログラミング学習"
+#puts webcamp
+
+#webcamp = "オンラインプログラミング学習"
+#puts webcamp
+
+#Pi = 3.14
+#puts Pi
+
+#Pi = 100
+#puts Pi
+
+#name = "A"
+#weight = 50
+
+#puts name + "さんの体重は"+ weight.to_s+"kgです"
+
+#puts "#{name}さんの体重は#{weight}kgです"
+#puts '#{name}さんの体重は#{weight}kgです'
+
+#names = ["Git", "HTML", "CSS"]
+#puts names[1]
+
+# = {"太郎"=>185,"二郎"=>170,"花子"=>150}
+#puts tall["太郎"]
