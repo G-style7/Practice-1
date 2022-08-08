dd2a438b82eacf210345802aeeee3f4d6ee352b5
EC2 Default User <ec2-user@ip-172-31-3-171.ap-northeast-1.compute.internal>
2022-08-08 14:38:52 +0000

    Ruby復習終了

---
+++ inheritance.rb
@@ -0,0 +1,25 @@
+#class Car
+#  def run(distance)
+ #   puts "車で#{distance}キロ走ります。"
+ # end
+#end
+
+#class Bus<Car
+#end
+
+#bus = Bus.new
+#bus.run(5)
+
+#puts Bus.superclass
+
+class Car
+ def run(distance)
+   puts "車で#{distance}キロ走ります。"
+ end
+end
+
+class Truck<Car
+end
+
+truck=Truck.new
+truck.run(5)
--- practice.rb
+++ practice.rb
@@ -147,16 +147,16 @@
 
 #def greeting(name)
  # return "Hello,#{name}"
-  
+
   #"Good morning,#{name}!"
-  
-#end 
+
+#end
 
 #puts greeting('John')
 
 
 #def calc(num)
  # return"計算できません" if num zero?
-  
+
 #end
 
