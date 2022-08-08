a6d4c4164bb0d30d4eb2c3d63e80155db55194e0
EC2 Default User <ec2-user@ip-172-31-3-171.ap-northeast-1.compute.internal>
2022-08-08 09:48:47 +0000

    第9章まで

--- practice.rb
+++ practice.rb
@@ -99,7 +99,7 @@
  #break
  #end
 
- #puts i 
+ #puts i
  #i+= 1
 #end
 
@@ -108,7 +108,7 @@
 #a=gets.to_i
 #b=gets.to_i
 
-#puts "計算結果を出力します"  
+#puts "計算結果を出力します"
 #puts "a*b=#{a*b}"
 #puts "計算を終了します"
 
@@ -121,13 +121,13 @@
 #while i<= input do
   #puts "#{i}回目の計算"
 #  puts "2つの値を入力してください"
-  
+
   #a = gets.to_i
 #  b = gets.to_i
-  
+
 #  puts "a=#{a}"
 #  puts "b=#{b}"
-  
+
 #  puts "計算結果を出力します"
 #  puts "a+b=#{a + b}"
  # puts "a-b=#{a - b}"
@@ -143,4 +143,20 @@
 #puts "WEBCAMP".reverse
 #puts "WEBCAMPでプログラミング学習".include?("WEBCAMP")
 #puts "WEBCAMPでプログラミング学習".methods
+
+
+#def greeting(name)
+ # return "Hello,#{name}"
+  
+  #"Good morning,#{name}!"
+  
+#end 
+
+#puts greeting('John')
+
+
+#def calc(num)
+ # return"計算できません" if num zero?
+  
+#end
 
